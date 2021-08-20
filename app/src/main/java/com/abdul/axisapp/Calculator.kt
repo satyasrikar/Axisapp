87c3708166e9a7a902527915d19c48256b9f74ef
Satyasri Kar <45929472+satyasrikar@users.noreply.github.com>
2021-08-20 11:44:03 +0530

    Division & Subtraction Methods added

--- app/src/main/java/com/abdul/axisapp/Calculator.kt
+++ app/src/main/java/com/abdul/axisapp/Calculator.kt
@@ -9,4 +9,17 @@ class Calculator {
     fun mul(a:Int, b:Int): Int{
         return a*b
     }
+
+    fun div(a:Int, b:Int): Int{
+        while(a!==0 && b!==0){
+            return a/b;
+        }
+        
+    }
+    fun sub(a:Int, b:Int): Int{
+        
+          return a-b;
+        
+        
+    }
 }
--- app/src/test/java/com/abdul/axisapp/CalculatorTest.kt
+++ app/src/test/java/com/abdul/axisapp/CalculatorTest.kt
@@ -13,10 +13,24 @@ class CalculatorTest : TestCase() {
         assertEquals(expected,actual)
     }
 
-    fun testSub(){
+    fun testMul(){
         var expected = 25
         var calculator = Calculator()
         var actual = calculator.mul(5,4)
+        assertEquals(expected,actual)
+    }
+
+      fun testDiv(){
+        var expected = 7
+        var calculator = Calculator()
+        var actual = calculator.div(21,3)
+        assertEquals(expected,actual)
+    }
+
+      fun testSub(){
+        var expected = 4
+        var calculator = Calculator()
+        var actual = calculator.div(25,1)
         assertEquals(expected,actual)
     }
 }
